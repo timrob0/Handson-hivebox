a6a18abc541a27f823185efda10d95bb39f0bba8
Tim Robinson Personal <timothy.robinson123@gmail.com>
2025-07-07 16:35:12 +1000

    Dockerfile changes to change ports and add in first api calls

--- docker-dynamicDevOp/main.py
+++ docker-dynamicDevOp/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+from fastapi import FastAPI
+
 def get_app_version() -> str:
     try:
         with open("version.txt", "r") as f:
             return f.read().strip()
     except FileNotFoundError:
         return "Unknown Version"
+
+
+app = FastAPI()
+
+@app.get("/version")
+def read_version():
+    return {"version": get_app_version()}
+
 
 def main():
     version = get_app_version()
